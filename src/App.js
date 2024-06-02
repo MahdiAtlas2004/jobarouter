34fc824e7304b975b1be95d07de5c6603e27a97f
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-02 19:43:30 +0430

    Routes updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+//layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Jobarouter</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">about</NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          {/* here index works as path="/" that indicates the root element */}
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
-export default App
+export default App;
