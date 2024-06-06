73dccc0d1d34efba28f3fa21a50fba404d586857
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-06 12:08:21 +0430

    Navigate component added to About to navigate user according to some condition.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const About = () => {
+  const [user, setUser] = useState("mario");
+
+  // Navigate component helps us to redirect user according to some specific condition.
+  if (!user) {
+    // the replace property replace the history of browser that not show About page while we go one page back
+    return <Navigate to="/" replace={true}/>;
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
@@ -22,6 +31,8 @@ const About = () => {
         optio! Quis culpa, in voluptatibus nihil eveniet voluptate unde omnis
         labore!
       </p>
+
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 };
